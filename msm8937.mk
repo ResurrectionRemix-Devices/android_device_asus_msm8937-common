TINNO-8937/android_device_tinno_msm8937-common

msm8937-common: Exclude lineage-sdk overlays from RRO
* lineage-sdk overlays don't work with RRO

Change-Id: Iff826faf1864388066a488acc9682636b98d1098

msm8937.mk
@@ -25,6 +25,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal

ResurrectionRemix-Devices/android_device_asus_msm8937-common

msm8937.mk
@@ -21,6 +21,8 @@ DEVICE_PACKAGE_OVERLAYS += $(VENDOR_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(VENDOR_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(VENDOR_PATH)/overlay-lineage/lineage-sdk
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal